1f43a7e89f303674a88872ecc9d50a64f001703d
Mattias Pernhult <pernhultmattias@gmail.com>
2016-01-12 21:58:15 +0100

    Added file that will modify the result.txt file

---
+++ file_watcher/exec/main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"math"
+	"math/rand"
+	"time"
+)
+
+func main() {
+	for {
+		random := getRandom()
+		text := fmt.Sprintf("%s,%v,%d\n", "3A", random, 20)
+		random = getRandom()
+		text += fmt.Sprintf("%s,%v,%d\n", "3B", random, 20)
+		random = getRandom()
+		text += fmt.Sprintf("%s,%v,%d\n", "3C", random, 20)
+
+		ioutil.WriteFile("../result.txt", []byte(text), 0644)
+
+		fmt.Println("Wrote:", random)
+
+		time.Sleep(5 * time.Second)
+	}
+}
+
+func getRandom() float64 {
+	random := math.Floor((rand.Float64() * 80) + 5)
+	return random
+}
